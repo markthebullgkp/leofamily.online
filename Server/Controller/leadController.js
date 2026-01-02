3a4f9f868b6e5c25ca0cbffd4603ee20b9f96f7f
markthebullgkp <120301477+markthebullgkp@users.noreply.github.com>
2026-01-02 12:21:26 +0530

    Add files via upload

---
+++ Server/Controller/leadController.js
@@ -0,0 +1,15 @@
+﻿import Lead from '../models/Lead.js';
+
+export const createLead = async (req, res) => {
+  const lead = await Lead.create(req.body);
+  res.json({
+    success: true,
+    message: '🔮 Birth chart request received!',
+    lead
+  });
+};
+
+export const getLeads = async (req, res) => {
+  const leads = await Lead.find().sort({ createdAt: -1 });
+  res.json(leads);
+};
